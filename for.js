6e37ccbc5d82bc3ef859134f38a54a33487f2734
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-01-10 13:55:02 -0300

    For classico

---
+++ for.js
@@ -0,0 +1,8 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+//primeira expressão: é execução apenas uma vez
+//Segundo expressão: condição de execução
+//Terceira expressão: é executada sempre ao final do bloco
+for (let indice = 0; indice < numeros.length; indice++) {
+	console.log(numeros);
+}
