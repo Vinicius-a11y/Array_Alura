b563f25f4097169605c567cbae363a39bc74f33d
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-01-15 09:02:45 -0300

    Alterando strings com map()

---
+++ assets/mapNomes.js
@@ -0,0 +1,7 @@
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+const nomesPadronizado = nomes.map((nomes) => {
+	return nomes.toLocaleUpperCase();
+});
+
+console.log(nomesPadronizado);
