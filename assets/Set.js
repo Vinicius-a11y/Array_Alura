744e096995662fc8487242fe42cbd1e58ff3dde7
Vinicius Bueno <BuenoVini455@hotmail.com>
2024-01-16 15:14:23 -0300

    Set e fim curso Arrays

---
+++ assets/Set.js
@@ -0,0 +1,16 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set([nomes]);
+
+const nomesAtt = [...meuSet];
+
+console.log(meuSet);
+
+// Como utilizar o filter e o reduce:
+// Aprendemos mais métodos modernos do JavaScript que nos ajudam em situações mais específicas. Utilizamos o filter para criar um array filtrado a partir de um existente e o reduce para acumular valores de um array em um só.
+
+// Como clonar arrays com o spread operator ...:
+// Entendemos o problema de referência do JavaScript ao tentar atribuir um array a um novo. Para resolver esse problema, utilizamos o spread operator para espalhar todos os valores de um array em um novo, realizando a clonagem com sucesso.
+
+// Como utilizar a estrutura Set:
+// Vimos como criar um Set a partir de um array existente, a fim de remover todas as suas duplicatas. Em seguida, utilizamos o spread operator para converter o Set de volta em um lista.
